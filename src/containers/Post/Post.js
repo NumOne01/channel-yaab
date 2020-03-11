470602c9cb127fb0c5fef80f8ed57fa6a64bde7b
Alireza <alireza.fz96@gmail.com>
2020-03-11 13:21:20 +0330

    completed post component

--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Spinner } from '../../components/UI'
 
 class Post extends Component {
+	state = {
+		post: null
+	}
+	componentDidMount() {
+		const post = this.props.posts.find(
+			post => post.key === this.props.match.params.id
+		)
+		this.setState({ post })
+	}
 	render() {
-		//console.log(this.props.match.params.id)
-		return <div>hell </div>
+		const { post } = this.state
+		return post ? (
+			<div>
+				<h2>{post.heading}</h2>
+				<p>{post.body}</p>
+			</div>
+		) : (
+			<Spinner />
+		)
 	}
 }
 
-export default Post
+const mapStateToProps = ({ posts }) => {
+	return { posts: posts.posts }
+}
+
+export default connect(mapStateToProps)(Post)
