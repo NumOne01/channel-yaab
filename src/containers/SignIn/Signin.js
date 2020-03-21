218cfd6312571633755c89c83709214717e94a09
Alireza <alireza.fz96@gmail.com>
2020-03-21 22:15:51 +0430

    fixed login page caused navbar to shrink

--- src/containers/SignIn/Signin.js
+++ src/containers/SignIn/Signin.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
@@ -23,9 +22,8 @@ function Copyright() {
 			{'Copyright © '}
 			<Link color="inherit" href="https://material-ui.com/">
 				channel-yaab
-			</Link>{' '}
+			</Link>
 			{new Date().getFullYear()}
-			{'.'}
 		</Typography>
 	)
 }
@@ -65,7 +63,6 @@ function SignIn(props) {
 		<Redirect to="/" />
 	) : (
 		<Container component="main" maxWidth="xs">
-			<CssBaseline />
 			<div className={classes.paper}>
 				<Avatar className={classes.avatar}>
 					<LockOutlinedIcon />
